b710daadf3c0505d2c9772569c553e0dde0703ef
maryperpetual <maryperpetual123@gmail.com>
2023-12-08 15:24:01 +0530

    done

---
+++ script.js
@@ -0,0 +1,7 @@
+// PRELOADER
+window.addEventListener("load", function() {  
+    setTimeout(function() {
+        document.querySelector(".preloader").style.display = "none";
+        document.querySelector(".content").style.display = "block";
+    }, 2000);
+});
